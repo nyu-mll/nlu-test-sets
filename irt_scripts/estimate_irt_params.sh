feb9bd603b9b2d94a4715506f28ba3f0c1affcd9
phu-pmh <phumon91@gmail.com>
2021-01-30 14:30:45 -0500

    estimate params

--- irt_scripts/estimate_irt_params.sh
+++ irt_scripts/estimate_irt_params.sh
@@ -1,19 +1,20 @@
 # BASE_DIR=/Users/phumon/Documents/Research/
-BASE_DIR=//Users/claravania/Projects
 # BASE_DIR=/scratch/wh629/research/irt/irt_script
-SCRIPT_DIR=${BASE_DIR}/nlu-test-sets/irt_scripts
+BASE_DIR=$(pwd)
+echo "Base dir: $BASE_DIR"
+SCRIPT_DIR=${BASE_DIR}/irt_scripts
 
-IN_DIR=${BASE_DIR}/nlu-test-sets/data
-OUT_DIR=${BASE_DIR}/nlu-test-sets/params
+IN_DIR=${BASE_DIR}/data
+OUT_DIR=${BASE_DIR}/params_mvirt
 SEED=101
 
 # adjust the following parameters according to chosen setup
 # here we use our best parameters used in the paper.
 # supported distributions: 'lognormal', 'beta', 'normal'
 DISTS=('lognormal')
-ALPH_STDS=('0.30')
+ALPH_STDS=('0.20' '0.15')
 PARAM_STDS=('1.0')
-
+DIMENSIONS=(90 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18)
 #  list of tasks to analyze
 TASKS="boolq,cb,commonsenseqa,copa,cosmosqa,hellaswag,rte,snli,wic,qamr,arct,mcscript,mctaco,mutual,mutual-plus,quoref,socialiqa,squad-v2,wsc,mnli,mrqa-nq,newsqa,abductive-nli,arc-easy,arc-challenge,piqa,quail,winogrande"
 
@@ -22,25 +23,28 @@ TASKS="boolq,cb,commonsenseqa,copa,cosmosqa,hellaswag,rte,snli,wic,qamr,arct,mcs
 sample_size=1500
 for alpha_std in "${ALPH_STDS[@]}"
 do
+    for dim in "${DIMENSIONS[@]}"
+    do
 	for item_std in "${PARAM_STDS[@]}"
 	do
-		echo Alpha Std $alpha_std, Diff Guess Std $item_std
-		ALPHA_TRANS=identity
-		THETA_TRANS=identity
-    python \
-			$SCRIPT_DIR/variational_irt.py \
-  			--response_dir $IN_DIR \
-  			--out_dir $OUT_DIR \
-  			--seed $SEED \
-  			--discr 'lognormal' \
-  			--ability 'normal' \
-  			--discr_transform $ALPHA_TRANS \
-  			--ability_transform $THETA_TRANS \
-        --datasets $TASKS \
-        --sample_size $sample_size \
-        --no_subsample \
-        --alpha_std $alpha_std \
-        --item_param_std $item_std \
-  			--verbose
+            echo Alpha Std $alpha_std, Diff Guess Std $item_std
+	    ALPHA_TRANS=identity
+	    THETA_TRANS=identity
+            python \
+		$SCRIPT_DIR/variational_irt.py \
+  		--response_dir $IN_DIR \
+  		--out_dir $OUT_DIR \
+  		--seed $SEED \
+  		--discr 'lognormal' \
+  		--ability 'normal' \
+  		--discr_transform $ALPHA_TRANS \
+  		--ability_transform $THETA_TRANS \
+               --datasets $TASKS \
+               --sample_size $sample_size \
+               --no_subsample \
+               --alpha_std $alpha_std \
+               --item_param_std $item_std \
+               --verbose
 	done
+    done
 done

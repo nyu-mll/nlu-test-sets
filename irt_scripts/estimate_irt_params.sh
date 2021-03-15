4fb394086cb89ac3204f278050efe8b2db4c1a71
phu-pmh <phumon91@gmail.com>
2021-03-15 19:46:48 -0400

    update data geneartion

--- irt_scripts/estimate_irt_params.sh
+++ irt_scripts/estimate_irt_params.sh
@@ -13,13 +13,13 @@ SEED=101
 # here we use our best parameters used in the paper.
 # supported distributions: 'lognormal', 'beta', 'normal'
 DISTS=('lognormal')
-ALPH_STDS=( '0.2' '0.4' '1' '8')
+ALPH_STDS=( '0.4' '0.2' )
 PARAM_STDS=('1.0' '2' '10')
 DIMENSIONS=( 1 2 3 4 5 6 7 8 ) ## 13 14 15 16 17 18 36 54 72)
-LR=(0.01) # 0.001 0.0001)
+LR=(0.0001 0.001)
 #  list of tasks to analyze
 #TASKS="boolq,cb,commonsenseqa,copa,cosmosqa,hellaswag,rte,snli,wic,qamr,arct,mcscript,mctaco,mutual,mutual-plus,quoref,socialiqa,squad-v2,wsc,mnli,mrqa-nq,newsqa,abductive-nli,arc-easy,arc-challenge,piqa,quail,winogrande"
-TASKS="sync_dim3_alpha-lognormal-2.00_theta-normal-2.00,sync_dim3_alpha-lognormal-7.00_theta-normal-10.00"
+TASKS="sync_dim3_mean0_alpha-lognormal-4.00_theta-normal-4.00,sync_dim3_mean10_alpha-lognormal-2.00_theta-normal-2.00"
 
 # if want to use sampling instead of all examples
 # not used if --no_subsample is specified
--- run_save_plots.py
+++ run_save_plots.py
@@ -5,7 +5,7 @@ import sys
 
 base_dir="/misc/vlgscratch4/BowmanGroup/pmh330/nlu-test-sets"
 save_dir="params_mvirt_sync"
-output_path="/misc/vlgscratch4/BowmanGroup/pmh330/nlu-test-sets/params_mvirt_sync/lr-*$*/params.p"
+output_path="/misc/vlgscratch4/BowmanGroup/pmh330/nlu-test-sets/params_mvirt_sync/lr-0.0001-steps-$5500-alpha-lognormal-identity-dim*_theta-normal-identity_nosubsample_*/params.p"
 for file_name in glob.glob(output_path):
     names = file_name.split('/')[-2].replace('$', '\$')
     dim = file_name.split("dim")[1].split("_")[0]
